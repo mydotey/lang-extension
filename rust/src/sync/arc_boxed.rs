c83c1ba6e34a561c287be8323ac400b28c485e4e
Qiang Zhao <koqizhao@outlook.com>
2019-10-20 16:25:39 +0800

    rust: enhance arc_boxed

--- rust/src/sync/arc_boxed.rs
+++ rust/src/sync/arc_boxed.rs
@@ -4,15 +4,21 @@ macro_rules! arc_boxed {
     ($value:tt) => {
         std::sync::Arc::new(Box::new($value))
     };
+    ($value:path) => {
+        std::sync::Arc::new(Box::new($value))
+    };
 }
 
 #[cfg(test)]
 mod tests {
     use crate::*;
+    use crate::fmt::*;
 
     #[test]
     fn arc_boxed() {
         let x = arc_boxed!(10);
         println!("{}", x);
+        let y = arc_boxed!(crate::sync::arc_boxed::tests::arc_boxed);
+        println!("{:?}", y.to_instance_string());
     }
 }
