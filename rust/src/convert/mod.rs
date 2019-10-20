0443bf155252a688f870c96cf5eea38cef2f0bd3
Qiang Zhao <koqizhao@outlook.com>
2019-10-20 21:18:47 +0800

    rust: add ValueFilter

--- rust/src/convert/mod.rs
+++ rust/src/convert/mod.rs
@@ -2,3 +2,6 @@
 #[macro_use]
 mod type_convert;
 pub use type_convert::*;
+
+mod value_filter;
+pub use value_filter::*;
---
+++ rust/src/convert/value_filter.rs
@@ -0,0 +1,91 @@
+use std::sync::Arc;
+use std::fmt;
+
+use crate::*;
+use crate::any::*;
+use crate::ops::function::*;
+
+pub trait RawValueFilter: Value + Sync + Send {
+    fn filter(&self, value: Box<dyn Value>) -> Option<Box<dyn Value>>;
+
+as_trait!(RawValueFilter);
+as_boxed!(RawValueFilter);
+}
+
+boxed_value_trait!(RawValueFilter);
+
+pub trait ValueFilter<V: ?Sized + ValueConstraint>: RawValueFilter {
+    fn filter(&self, value: Box<V>) -> Option<Box<V>>;
+}
+
+#[derive(Clone)]
+pub struct DefaultValueFilter<V: ?Sized + ValueConstraint> {
+    filter: Function<Box<V>, Option<Box<V>>>
+}
+
+impl<V: ?Sized + ValueConstraint> DefaultValueFilter<V> {
+    pub fn new(filter: Box<dyn Fn(Box<V>) -> Option<Box<V>>>) -> Self {
+        Self::wrap(Arc::new(filter))
+    }
+
+    pub fn wrap(filter: Arc<Box<dyn Fn(Box<V>) -> Option<Box<V>>>>) -> Self {
+        Self {
+            filter: filter
+        }
+    }
+}
+
+impl<V: ?Sized + ValueConstraint> ValueFilter<V>
+    for DefaultValueFilter<V> {
+    fn filter(&self, value: Box<V>) -> Option<Box<V>> {
+        self.filter.as_ref()(value)
+    }
+}
+
+impl<V: ?Sized + ValueConstraint> PartialEq for DefaultValueFilter<V> {
+    fn eq(&self, other: &Self) -> bool {
+        self.filter.as_ref().reference_equals(other.filter.as_ref())
+    }
+}
+
+impl<V: ?Sized + ValueConstraint> Eq for DefaultValueFilter<V> { }
+
+impl<V: ?Sized + ValueConstraint> fmt::Debug for DefaultValueFilter<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{ filter: {} }}", self.filter.type_name())
+    }
+}
+
+unsafe impl<V: ?Sized + ValueConstraint> Sync for DefaultValueFilter<V> { }
+unsafe impl<V: ?Sized + ValueConstraint> Send for DefaultValueFilter<V> { }
+
+impl<V: ?Sized + ValueConstraint> RawValueFilter for DefaultValueFilter<V> {
+    fn filter(&self, value: Box<dyn Value>) -> Option<Box<dyn Value>> {
+        match value.as_ref().as_any_ref().downcast_ref::<V>() {
+            Some(value) => self.filter.as_ref()(Box::new(value.clone())).map(|v|Value::clone_boxed(v.as_ref())),
+            None => None
+        }
+    }
+
+as_trait!(impl RawValueFilter);
+as_boxed!(impl RawValueFilter);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn value_filter_test() {
+        let filter = DefaultValueFilter::<i32>::new(Box::new(|v|{
+            if *v > 10 { Some(v) } else { None }
+        }));
+        let v = ValueFilter::<i32>::filter(&filter, Box::new(10));
+        assert_eq!(None, v);
+        let v = ValueFilter::<i32>::filter(&filter, Box::new(11));
+        assert_eq!(Box::new(11), v.unwrap());
+        let v = RawValueFilter::filter(&filter, Box::new(11));
+        let expected: Box<dyn Value> = Box::new(11);
+        assert_eq!(&expected, &v.unwrap());
+    }
+}
